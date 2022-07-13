0e65f1f42b42d9184afbda0314d9d5d4d079426f
wujinwen <wujinwen@leinao.ai>
2022-07-13 10:09:12 +0800

    change

--- js/proxy.js
+++ js/proxy.js
@@ -1,8 +1,49 @@
-function handler() {
-    // ?
+// const handler = {
+//     get: function(target, key, receiver) {
+//         return Reflect.get(target, key, receiver)
+//     }
+// }
+
+
+const handler2 = {
+    get: function(target, key, receiver) {
+        return target[key]
+    }
 }
 
-function reactive (target) {
+/**
+ * 
+ * @param {*} target 
+ * @param {*} flag  true: use Reflect  false: use default
+ * @returns 
+ */
+function reactive(target, flag) {
     // ....
-    return new Proxy(target, handler);
+    return new Proxy(target, flag ? handler : handler2);
+}
+
+
+const person = {
+    name: 'Tim',
+    age: 34,
+    get getName () {
+        return this.name
+    }
+}
+
+const child = {
+    __proto__: person,
+    name: 'Child',
+}
+
+
+const reactiveChildByReflect = reactive(child, true)
+
+const reactiveChild = reactive(child, false)
+
+console.log(reactiveChild.getName)
+console.log(reactiveChildByReflect.getName)
+
+const handler = {
+    // ?
 }
